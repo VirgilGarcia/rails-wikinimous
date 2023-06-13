b87ac80818cdd148f69dd0e1cfb5097ac264852c
Virgil Garcia <13viga@gmail.com>
2023-06-13 15:48:52 +0200

    rails wikinimous

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,11 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get 'articles', to: 'articles#index'
+  get "articles/new", to: "articles#new"
+  delete "articles/:id", to: "articles#destroy", as: :delete
+  get "articles/:id/edit", to: "articles#edit", as: :edit
+  patch "articles/:id", to: "articles#update", as: :update_article
+  get 'articles/:id', to: 'articles#show', as: :article
+  post "articles", to: "articles#create"
 end
